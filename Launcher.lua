385480ef586511d7d224bf955ee6534981f0423f
Lua <luahubexploits@gmail.com>
2022-08-14 14:38:56 +0100

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -7,4 +7,6 @@ end
 
 if game.PlaceId == 286090429 then
     load("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/Arsenal.lua")
+else if game.PlaceId == 2248408710 then
+    load("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/Destruction.lua")
 end
