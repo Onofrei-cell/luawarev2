e1342cb7e756ab21e5bc1639bdec594137fdd857
Lua <luahubexploits@gmail.com>
2022-08-21 20:41:33 +0100

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,9 +1,13 @@
-function load(link)
-    loadstring(game:HttpGet((link),true))()
+print("Luaware V2 - "..game.Name)
+
+function loadscript(url)
+    loadstring(game:HttpGet((url),true))()
 end
 
-if game.PlaceId == 286090429 then
-    load("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/Arsenal.lua")
-else if game.PlaceId == 2248408710 then
-    load("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/Destruction.lua")
+if game.GameId == 2551991523 then -- Broken Bones IV
+    loadscript("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/BrokenBones.lua")
+elseif game.GameId == 2248408710 then -- Destruction Simulator
+    loadscript("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/Destruction.lua")
+elseif game.GameId == 286090429 then -- Arsenal
+    loadscript("https://raw.githubusercontent.com/lua-ware/luawarev2/main/Scripts/Arsenal.lua")
 end
